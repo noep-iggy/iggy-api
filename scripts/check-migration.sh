6caa9b4b58a855b41587c2301940d35965913e7a
Noé <noe.philippe@bob-le-developpeur.com>
2024-01-17 14:52:38 +0100

    ✨ Create script for prod

---
+++ scripts/check-migration.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Vérifiez s'il y a des migrations en attente
+pending_migrations=$(npx typeorm migration:show -d ./ormconfig.ts)
+
+if [ -z "$pending_migrations" ]; then
+    echo "No pending migrations"
+else
+    echo "Pending migrations found"
+    
+    # Vérifiez si les migrations ont déjà été générées
+    if [ ! -d "./src/migrations" ]; then
+        echo "Generating migrations"
+        npm run migrate:generate || exit 1
+    else
+        # Assurez-vous que le répertoire contient des fichiers de migration
+        num_migrations=$(ls ./src/migrations/*.ts 2>/dev/null | wc -l)
+        if [ $num_migrations -eq 0 ]; then
+            echo "Generating migrations"
+            npm run migrate:generate || exit 1
+        fi
+    fi
+    
+    # Exécutez les migrations
+    npm run migrate:run || exit 1
+fi
+
+# Démarrer l'application
+node dist/src/main
