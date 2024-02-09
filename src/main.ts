3a79dfd8d55251d2c8a8258253eb5e38a3e023e8
Noé <noe.philippe@bob-le-developpeur.com>
2024-02-09 09:36:09 +0100

    ✨ Disable whitelist

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,25 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const whitelist = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://localhost:8081',
-    'http://noephilippe.freeboxos.fr:8000',
-    'http://noephilippe.freeboxos.fr:3000',
-    'http://10.135.129.143:8081',
-  ];
+  // const whitelist = [
+  //   'http://localhost:3000',
+  //   'http://localhost:8000',
+  //   'http://localhost:8081',
+  //   'http://noephilippe.freeboxos.fr:8000',
+  //   'http://noephilippe.freeboxos.fr:3000',
+  //   'http://10.135.129.143:8081',
+  // ];
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     if (!origin || whitelist.indexOf(origin) !== -1) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  // });
   app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*',
   });
   await app.listen(process.env.API_PORT || 8000);
 }
