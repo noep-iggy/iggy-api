42aa424b7b40be50ea7dbb673940bc0815e7997a
Noé <noe.philippe@bob-le-developpeur.com>
2024-03-08 23:40:31 +0100

    🐛 fix CORS

--- src/main.ts
+++ src/main.ts
@@ -3,35 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const whitelist = [
-  //   'http://localhost:3000',
-  //   'http://localhost:8000',
-  //   'http://localhost:8081',
-  //   'http://noephilippe.freeboxos.fr:8000',
-  //   'http://noephilippe.freeboxos.fr:3000',
-  //   'http://10.135.129.143:8081',
-  //   'https://backoffice.cocon-animal.fr',
-  //   'https://api.cocon-animal.fr',
-  //   'https://www.cocon-animal.fr',
-  //   'https://cocon-animal.fr',
-  //   'https://www.api.cocon-animal.fr',
-  //   'https://www.backoffice.cocon-animal.fr',
-  // ];
-  // app.enableCors({
-  //   origin: function (origin, callback) {
-  //     if (!origin || whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   allowedHeaders:
-  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-  //   credentials: true,
-  // });
   app.enableCors({
-    origin: '*',
+    origin: '*', // Autorise toutes les origines, vous pouvez spécifier des origines spécifiques au besoin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Méthodes HTTP autorisées
+    allowedHeaders: 'Content-Type,Authorization', // En-têtes autorisés
+    exposedHeaders: 'Content-Length, X-Request-ID', // En-têtes exposés
+    credentials: true, // Autorise l'envoi de cookies avec la requête (pour les requêtes avec credentials)
+    maxAge: 600, // Temps de mise en cache (en secondes) de la pré-vérification (preflight)
   });
   await app.listen(process.env.API_PORT || 8000);
 }
