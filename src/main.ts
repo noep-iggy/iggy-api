0cc803fc71751b2d1edb27d5970d0ff8b1164868
Noé <noe.philippe@bob-le-developpeur.com>
2024-03-08 22:18:37 +0100

    🐛 fix CORS

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,28 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const whitelist = [
-  //   'http://localhost:3000',
-  //   'http://localhost:8000',
-  //   'http://localhost:8081',
-  //   'http://noephilippe.freeboxos.fr:8000',
-  //   'http://noephilippe.freeboxos.fr:3000',
-  //   'http://10.135.129.143:8081',
-  // ];
-  // app.enableCors({
-  //   origin: function (origin, callback) {
-  //     if (!origin || whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  // });
+  const whitelist = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:8081',
+    'http://noephilippe.freeboxos.fr:8000',
+    'http://noephilippe.freeboxos.fr:3000',
+    'http://10.135.129.143:8081',
+    'https://backoffice.cocon-animal.fr',
+    'https://api.cocon-animal.fr',
+  ];
   app.enableCors({
-    origin: '*',
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
   });
+  // app.enableCors({
+  //   origin: '*',
+  // });
   await app.listen(process.env.API_PORT || 8000);
 }
 bootstrap();
