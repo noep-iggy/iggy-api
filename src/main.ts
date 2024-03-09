1efb51baa2741537b6290223a79e791a7e652cf2
Noé <noe.philippe@bob-le-developpeur.com>
2024-03-09 09:36:34 +0100

    ✨ Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Autorise toutes les origines, vous pouvez spécifier des origines spécifiques au besoin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Méthodes HTTP autorisées
+    allowedHeaders: 'Content-Type,Authorization', // En-têtes autorisés
+    exposedHeaders: 'Content-Length, X-Request-ID', // En-têtes exposés
+    credentials: true, // Autorise l'envoi de cookies avec la requête (pour les requêtes avec credentials)
+    maxAge: 600, // Temps de mise en cache (en secondes) de la pré-vérification (preflight)
+  });
   await app.listen(process.env.API_PORT || 8000);
 }
 bootstrap();
